e5910f69916476eb1d489883626b3c4197f1b274
Bennett <theagentwo@gmail.com>
2025-11-21 13:16:45 -0500

    a

---
+++ netlify/functions/maps-proxy.js
@@ -0,0 +1,2 @@
+// This file is deprecated and should be ignored.
+// The build process compiles netlify/functions/maps-proxy.ts to functions-dist/maps-proxy.js
---
+++ netlify/functions/process-analysis-job.js
@@ -0,0 +1,2 @@
+// This file is deprecated and should be ignored.
+// The build process compiles netlify/functions/process-analysis-job.ts to functions-dist/process-analysis-job.js
