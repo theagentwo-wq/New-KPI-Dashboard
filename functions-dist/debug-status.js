f4bdf165c43ae6bcdba0a40209f22345a083ebdb
Bennett <theagentwo@gmail.com>
2025-11-22 00:41:28 -0500

    g

--- functions-dist/debug-status.js
+++ functions-dist/debug-status.js
@@ -1,4 +1,28 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
 // netlify/functions/debug-status.ts
+var debug_status_exports = {};
+__export(debug_status_exports, {
+  handler: () => handler
+});
+module.exports = __toCommonJS(debug_status_exports);
 var handler = async (event, context) => {
   return {
     statusCode: 200,
@@ -13,6 +37,7 @@ var handler = async (event, context) => {
     })
   };
 };
-export {
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
   handler
-};
+});
--- functions-dist/rss-proxy.js
+++ functions-dist/rss-proxy.js
@@ -1,4 +1,28 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
 // netlify/functions/rss-proxy.ts
+var rss_proxy_exports = {};
+__export(rss_proxy_exports, {
+  handler: () => handler
+});
+module.exports = __toCommonJS(rss_proxy_exports);
 var RSS_FEEDS = [
   { name: "Restaurant Business", url: "https://www.restaurantbusinessonline.com/rss/headlines" },
   { name: "Nation's Restaurant News", url: "https://www.nrn.com/rss.xml" },
@@ -102,6 +126,7 @@ var handler = async () => {
     };
   }
 };
-export {
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
   handler
-};
+});
