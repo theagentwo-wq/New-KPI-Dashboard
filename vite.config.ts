5f8a0a22d95711250501e99d18761519ef339b97
theagentwo <theagentwo@gmail.com>
2025-11-24 04:38:25 +0000

    44

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './'),
+        '@': path.resolve(__dirname, './src'),
       },
     },
     server: {
